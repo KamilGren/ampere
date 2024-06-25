6fa9f6ce539be3bab3376d86d612c03fbb70a9e9
Madizyn <145222821+madimadica@users.noreply.github.com>
2024-06-24 22:32:07 -0500

    Add /buildings view

--- oze_app/src/main/java/pl/gren/oze_app/controller/BuildingInfoController.java
+++ oze_app/src/main/java/pl/gren/oze_app/controller/BuildingInfoController.java
@@ -1,14 +1,36 @@
 package pl.gren.oze_app.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import pl.gren.oze_app.model.db.entity.BuildingInfo;
+import pl.gren.oze_app.model.db.entity.Client;
 import pl.gren.oze_app.model.db.enums.*;
+import pl.gren.oze_app.model.db.repository.BuildingInfoRepository;
+
+import java.util.List;
 
 @Controller
+@RequestMapping("buildings")
 public class BuildingInfoController {
 
-    @GetMapping("/building-info/new")
+    private final BuildingInfoRepository buildingInfoRepository;
+
+    @Autowired
+    public BuildingInfoController(BuildingInfoRepository buildingInfoRepository) {
+        this.buildingInfoRepository = buildingInfoRepository;
+    }
+
+    @GetMapping("")
+    public String viewAllBuildings(Model model) {
+        List<BuildingInfo> buildings = buildingInfoRepository.findAll();
+        model.addAttribute("rows", buildings);
+        return "buildings/index";
+    }
+
+    @GetMapping("/old-demo")
     public String form(Model model) {
         addTypeEnums(model);
         return "building_info/form";
--- oze_app/src/main/java/pl/gren/oze_app/model/db/repository/BuildingInfoRepository.java
+++ oze_app/src/main/java/pl/gren/oze_app/model/db/repository/BuildingInfoRepository.java
@@ -4,9 +4,11 @@ import org.springframework.stereotype.Repository;
 import pl.gren.oze_app.model.db.entity.BuildingInfo;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface BuildingInfoRepository extends CrudRepository<BuildingInfo, Long> {
+    List<BuildingInfo> findAll();
     Optional<BuildingInfo> findByLocation(String location);
 }
